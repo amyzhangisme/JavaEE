88bf513d237b6220c5c461f2bd6d3cb9a2667790
amyzhangisme <amy.zhangisme@gmail.com>
2016-09-18 10:49:23 +0800

    m
    new

--- untitled/client2.py
+++ untitled/client2.py
@@ -2,7 +2,7 @@
 import socket
 ANY = socket.gethostbyname('localhost')
 HOST='127.0.0.1'
-PORT=3333
+PORT=3332
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      #定义socket类型，网络通信，TCP
 s.connect((HOST,PORT))       #要连接的IP与端口
 while 1:
@@ -11,6 +11,6 @@ while 1:
        data=s.recv(1024)     #把接收的数据定义为变量
        print data
        if(data=='!'):
-              print '您已离开'  # 输出变量
+              print '服务器已关闭'  # 输出变量
               s.close()
 s.close()   #关闭连接
--- untitled/server.py
+++ untitled/server.py
@@ -1,22 +1,45 @@
 # -*- coding: utf-8 -*-：
 import socket   #socket模块
+import threading
 import commands   #执行系统命令模块
-HOST='10.0.0.27'
-PORT=1501
+HOST='127.0.0.1'
+PORT=3333
 ANY = socket.gethostbyname('localhost')
+
+def jonnyS(client, address):
+    try:
+    #设置超时时间
+        client.settimeout(500)
+    except socket.timeout:
+        print 'time out'
+    print'Connected by', addr  # 输出客户端的IP地址
+    while 1:
+        c=''
+        data = client.recv(1024)  # 把接收的数据实例化
+        print client
+        print data
+        if(data=='exit'):
+            client.close()
+        client.sendall(data[::-1])
+
+    # #将接收到的信息原样的返回到客户端中
+    #     client.send(buf)
+    #超时后显示退出
+
+    #关闭与客户端的连接
+    client.close()
+
+
 s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)   #定义socket类型，网络通信，TCP
-hello = 'Hello world!'
 print'Connected by'
-s.bind((ANY,PORT))   #套接字绑定的IP与端口
+s.bind((HOST,PORT))   #套接字绑定的IP与端口
 print'Connected by'
-s.listen(1)         #开始TCP监听
+s.listen(20)         #开始TCP监听
 while 1:
        conn,addr=s.accept()   #接受TCP连接，并返回新的套接字与IP地址
-       print'Connected by',addr    #输出客户端的IP地址
-       while 1:
-                data=conn.recv(1024)    #把接收的数据实例化
-                print data
+       thread = threading.Thread(target=jonnyS, args=(conn,addr))
+       thread.start()
 
-                conn.sendall('!dlrow olleH')
-                
 conn.close()     #关闭连接
+
+
