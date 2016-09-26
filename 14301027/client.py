839bb1a8ec7fc51818891a69d9d5ee95c780b800
amyzhangisme <amy.zhangisme@gmail.com>
2016-09-26 22:51:22 +0800

    new
    jdhfk

--- 14301027/client.py
+++ 14301027/client.py
@@ -2,12 +2,12 @@
 import socket
 ANY = socket.gethostbyname('localhost')
 HOST='127.0.0.1'
-PORT=3333
+PORT=3326
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)      #定义socket类型，网络通信，TCP
 s.connect((HOST,PORT))       #要连接的IP与端口
 while 1:
        cmd=raw_input("Please input(exit中止对话):")       #与人交互，输入命令
-       s.sendall(cmd)      #把命令发送给对端
+       s.sendall("Get /foo.php HTTP/1.1\r\nfirst_name=John&last_name=Doe&action=Submit")      #把命令发送给对端
        data=s.recv(1024)     #把接收的数据定义为变量
        print data
        if(data=='!'):
